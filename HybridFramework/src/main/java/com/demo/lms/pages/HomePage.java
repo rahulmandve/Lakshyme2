b0481a62b107882f66dd09f062675194d828649d
dell <dell@DESKTOP-P1VUD1T>
2020-01-05 01:34:20 -0600

    secondCommit

--- HybridFramework/src/main/java/com/demo/lms/pages/HomePage.java
+++ HybridFramework/src/main/java/com/demo/lms/pages/HomePage.java
@@ -1,7 +1,5 @@
 package com.demo.lms.pages;
 
-import java.awt.List;
-
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
